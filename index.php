3a12538e9642d48927df18cff7b82a113ca5c9fa
khier <wog.kcpetalio@gmail.com>
2022-04-09 15:43:25 +0800

    [UPDATE]

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
 <meta charset="utf-8">
 <title>Drag and Drop</title>
-<!-- <link rel="stylesheet" href="../bootstrap/css/bootstrap.css"> -->
 <script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.7.2.custom.min.js"></script>
 </head>
@@ -39,16 +38,7 @@ $(document).ready(function(){
 
 </script>
 <body>
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'UA-49240280-1', 'url.ph');
-	  ga('send', 'pageview');
-
-	</script>
 <?php
 error_reporting(0);
 $mysqli = new mysqli("localhost","root","","demo");
